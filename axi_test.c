9eab85b8f7e3232afd50e192aaf863bc0950e3c9
ElectronAsh <adevans2k3@hotmail.com>
2020-10-10 04:16:43 +0100

    added GPU command manager

--- axi_test.c
+++ axi_test.c
@@ -43,10 +43,70 @@
 #define HW_FPGA_FB_SPAN (0x00400000) 		// Bridge span 4MB
 #define HW_FPGA_FB_MASK ( HW_FPGA_FB_SPAN - 1 )
 
-uint8_t mmap_setup_done = 0;
-
 void *axi_addr;
 void *fb_addr;
+
+/*
+ADR +0 = Write / Read to GP0
+ADR +4 = Write / Read to GP1
+
+ADR +8 = Bit 31 DMA_ACK (Write)
+		 Bit 30 DMA_REQ Read / gpu_nrst (Write). Must keep gpu_nrst bit HIGH when writing to DMA_ACK!
+		 Bit 29 IRQRequest Read 
+         Bit 28 dbg_canWrite
+		 Bit 27:0 myDebugCnt (GPU counter cycle)
+
+ADR +12= Read Data bus (cpuDataOut), without any other CPU signal.
+         Write Data bus (cpuDataIn) + DMA_ACK = true (pulse).
+*/
+
+class GPUManager {
+public:
+	GPUManager(uint32_t* gpuBase):BASE_GPU(gpuBase),diff(0),writeIdx(0),readIdx(0) {}
+	
+	bool canPush      () { return (BASE_GPU[2] & 1<<28);    }		// Read dbg_canWrite flag.
+	uint32_t  getGPUCycle  () { return BASE_GPU[2] & 0x0FFFFFFF; }	// Read myDebugCnt.
+
+	void StartGPUReset() { BASE_GPU[2] = 0<<30;                 }
+	void EndGPUReset  () { BASE_GPU[2] = 1<<30;                 }
+	
+	bool canWriteCommand() {
+		return (diff < MAX_SIZE-2);
+	}
+	
+	void writeCommand (uint32_t v) {
+		// User must check to know, but I check here to avoid memory overwrite.
+		if (canWriteCommand()) {
+			diff++;
+			buffer[writeIdx++] = v;
+			if (writeIdx >= MAX_SIZE) { writeIdx = 0; }
+		}
+	}
+	
+	void executeInLoop() {
+		if (canPush() && diff) {
+			writeGP0(buffer[readIdx++]);
+			if (readIdx >= MAX_SIZE) { readIdx = 0; }
+			diff--;
+		}
+	}
+	
+private:
+	static const uint32_t MAX_SIZE = 4096;
+	
+	void writeGP0(uint32_t value) { BASE_GPU[0] = value; }
+	void writeGP1(uint32_t value) { BASE_GPU[1] = value; }
+
+	uint32_t* BASE_GPU;
+	uint32_t diff;
+	uint32_t readIdx;
+	uint32_t writeIdx;
+	uint32_t buffer[MAX_SIZE];
+};
+
+
+uint8_t mmap_setup_done = 0;
+
 
 int mmap_setup() {
 	printf("HPS AXI Bridge Setup. ElectronAsh / dentnz\n\n");
@@ -101,17 +161,12 @@ int mmap_setup() {
 
 int main()
 {
-	uint32_t reg0;
-	uint32_t reg1;
-	uint32_t reg2;
-	uint32_t reg3;
-	uint32_t reg4;
-	uint32_t reg5;
-	uint32_t reg6;
-	uint32_t reg7;
+	uint32_t reg0, reg1, reg2, reg3, reg4, reg5, reg6, reg7;
 
 	mmap_setup();
 
+	GPUManager mgr( ((uint32_t *)axi_addr) );
+	
 /*	
 ADR +0 = Write / Read to GP0
 ADR +4 = Write / Read to GP1
@@ -130,40 +185,138 @@ ADR +12= Read Data bus (cpuDataOut), without any other CPU signal.
 
 	// Clear the GPU framebuffer with a known value before starting each test.
 	for (int i=0; i<262144; i++) {
-		*((uint32_t *)fb_addr+i) = 0x001F001F;
+		*((uint32_t *)fb_addr+i) = 0x04000400;
 	}
 	
 	printf("Resetting the GPU core...\n");
+	/*
 	*((uint32_t *)axi_addr+0x000000002) = 0x00000000; // Write to DMA_ACK / gpu_nrst. Assert gpu_nrst (LOW).
 	usleep(20000);
 	*((uint32_t *)axi_addr+0x000000002) = 0x40000000; // Write to DMA_ACK / gpu_nrst. Bring gpu_nrst HIGH again.
 	usleep(20000);
+	*/
 	
-	// Write to GP0...
-	*((uint32_t *)axi_addr+0x000000000) = 0xE100020A;	// Texpage.
-	*((uint32_t *)axi_addr+0x000000000) = 0xE2000000;	// Texwindow.
-	*((uint32_t *)axi_addr+0x000000000) = 0xE3000000;	// DrawAreaX1Y1.
-	*((uint32_t *)axi_addr+0x000000000) = 0xE4077E7F;	// DrawAreaX2Y2.
-	*((uint32_t *)axi_addr+0x000000000) = 0xE5000000;	// DrawAreaOffset.
-	*((uint32_t *)axi_addr+0x000000000) = 0xE6000000;	// MaskBits.
+	mgr.StartGPUReset(); // First thing to do in the morning, brush your teeth.
+	mgr.EndGPUReset();   // Toilet, then breakfast.
+
+	mgr.writeCommand(0x02FFFFFF);
+	mgr.writeCommand(0x00000000);
+	mgr.writeCommand(0x00040010);
+	for (int i=0; i<3; i++) mgr.executeInLoop();
+	
+	/*
+	mgr.writeCommand(0x30FF0000);
+	mgr.writeCommand(0x00000000);
+	mgr.writeCommand(0x0000FF00);
+	mgr.writeCommand(0x0000000F);
+	mgr.writeCommand(0x000000FF);
+	mgr.writeCommand(0x000F000F);
+	for (int i=0; i<6; i++) mgr.executeInLoop();
+	*/
 	
 	// Write to GP0...
 	/*
-	*((uint32_t *)axi_addr+0x000000000) = 0x02FFFFFF; // Fill Rect WHITE
-	*((uint32_t *)axi_addr+0x000000000) = 0x00000000; // Start pos 0,0
-	*((uint32_t *)axi_addr+0x000000000) = 0x00100010; // Size 16x16 pixel.
+	mgr.writeCommand(0xE100020A;	// Texpage.
+	mgr.writeCommand(0xE2000000;	// Texwindow.
+	mgr.writeCommand(0xE3000000;	// DrawAreaX1Y1.
+	mgr.writeCommand(0xE4077E7F;	// DrawAreaX2Y2.
+	mgr.writeCommand(0xE5000000;	// DrawAreaOffset.
+	mgr.writeCommand(0xE6000000;	// MaskBits.
+	for (int i=0; i<6; i++) mgr.executeInLoop();
+	*/
+
+	// Test poly RGB.
+	/*
+	mgr.writeCommand(0x30FF0000;	// (CcBbGgRrh)  Color1+Command.  (blue) Shaded three-point poly. 
+	mgr.writeCommand(0x00000000;	// (YyyyXxxxh)  Vertex 1.  Y=0. X=0
+	mgr.writeCommand(0x0000FF00;	// (00BbGgRrh)  Color2. (green)
+	mgr.writeCommand(0x0000000F;	// (YyyyXxxxh)  Vertex 2.  Y=0. X=15
+	mgr.writeCommand(0x000000FF;	// (00BbGgRrh)  Color3. (red)
+	mgr.writeCommand(0x000F000F;	// (YyyyXxxxh)  Vertex 3.  Y=15. X=15
+	for (int i=0; i<6; i++) mgr.executeInLoop();
+	*/
+	
+	// Test poly white.
+	/*
+	mgr.writeCommand(0x30FFFFFF;	// (CcBbGgRrh)  Color1+Command.  (blue) Shaded three-point poly. 
+	mgr.writeCommand(0x00000000;	// (YyyyXxxxh)  Vertex 1.  Y=0. X=0
+	mgr.writeCommand(0x00FFFFFF;	// (00BbGgRrh)  Color2. (green)
+	mgr.writeCommand(0x0000000F;	// (YyyyXxxxh)  Vertex 2.  Y=0. X=15
+	mgr.writeCommand(0x00FFFFFF;	// (00BbGgRrh)  Color3. (red)
+	mgr.writeCommand(0x000F000F;	// (YyyyXxxxh)  Vertex 3.  Y=15. X=15
+	for (int i=0; i<6; i++) mgr.executeInLoop();
+	*/
+	
+	// Test poly red.
+	/*
+    mgr.writeCommand(0x300000FF;    // (CcBbGgRrh)  Color1+Command. Shaded three-point poly. 
+    mgr.writeCommand(0x00000000;    // (YyyyXxxxh)  Vertex 1.  Y=0. X=0
+    mgr.writeCommand(0x000000FF;    // (00BbGgRrh)  Color2.
+    mgr.writeCommand(0x0000000F;    // (YyyyXxxxh)  Vertex 2.  Y=0. X=15
+    mgr.writeCommand(0x000000FF;    // (00BbGgRrh)  Color3.
+    mgr.writeCommand(0x000F000F;    // (YyyyXxxxh)  Vertex 3.  Y=15. X=15
+	for (int i=0; i<6; i++) mgr.executeInLoop();
+	*/
+	
+	// Test poly green.
+	/*
+	mgr.writeCommand(0x3000FF00;	// (CcBbGgRrh)  Color1+Command. Shaded three-point poly. 
+	mgr.writeCommand(0x00000000;	// (YyyyXxxxh)  Vertex 1.  Y=0. X=0
+	mgr.writeCommand(0x0000FF00;	// (00BbGgRrh)  Color2.
+	mgr.writeCommand(0x0000000F;	// (YyyyXxxxh)  Vertex 2.  Y=0. X=15
+	mgr.writeCommand(0x0000FF00;	// (00BbGgRrh)  Color3.
+	mgr.writeCommand(0x000F000F;	// (YyyyXxxxh)  Vertex 3.  Y=15. X=15
+	for (int i=0; i<6; i++) mgr.executeInLoop();
+	*/
+	
+	// Test poly blue.
+	/*
+	mgr.writeCommand(0x30FF0000;	// (CcBbGgRrh)  Color1+Command. Shaded three-point poly. 
+	mgr.writeCommand(0x00000000;	// (YyyyXxxxh)  Vertex 1.  Y=0. X=0
+	mgr.writeCommand(0x00FF0000;	// (00BbGgRrh)  Color2.
+	mgr.writeCommand(0x0000000F;	// (YyyyXxxxh)  Vertex 2.  Y=0. X=15
+	mgr.writeCommand(0x00FF0000;	// (00BbGgRrh)  Color3.
+	mgr.writeCommand(0x000F000F;	// (YyyyXxxxh)  Vertex 3.  Y=15. X=15
+	for (int i=0; i<6; i++) mgr.executeInLoop();
+	*/
+	
+	// Write to GP0...
+	/*
+	mgr.writeCommand(0x02FFFFFF; // Fill Rect WHITE
+	mgr.writeCommand(0x00000000; // Start pos 0,0
+	mgr.writeCommand(0x00100010; // Size 16x16 pixel.
+	for (int i=0; i<3; i++) mgr.executeInLoop();
 	*/
 	
 	/*
-	*((uint32_t *)axi_addr+0x000000000) = 0x02FFFFFF; // Fill Rect WHITE
-	*((uint32_t *)axi_addr+0x000000000) = 0x00400040; // Start pos 64,64
-	*((uint32_t *)axi_addr+0x000000000) = 0x00400040; // Size 64x64 pixel.
+	mgr.writeCommand(0x02FFFFFF; // Fill Rect WHITE
+	mgr.writeCommand(0x00400040; // Start pos 64,64
+	mgr.writeCommand(0x00400040; // Size 64x64 pixel.
+	for (int i=0; i<3; i++) mgr.executeInLoop();
 	*/
 	
 	/*
-	*((uint32_t *)axi_addr+0x000000000) = 0x02FFFFFF; // Fill Rect WHITE
-	*((uint32_t *)axi_addr+0x000000000) = 0x00000000; // Start pos 0,0
-	*((uint32_t *)axi_addr+0x000000000) = 0x01ff03ff; // Size 511x1023 pixel.
+	mgr.writeCommand(0x02FFFFFF; // Fill Rect WHITE
+	mgr.writeCommand(0x00000000; // Start pos 0,0
+	mgr.writeCommand(0x01ff03ff; // Size 511x1023 pixel.
+	for (int i=0; i<3; i++) mgr.executeInLoop();
+	*/
+	
+	/*
+	mgr.writeCommand(0x020000FF; // Fill Rect RED.
+	mgr.writeCommand(0x00000000; // Start pos 0,0
+	mgr.writeCommand(0x00400040; // Size 64x64 pixel.
+	for (int i=0; i<3; i++) mgr.executeInLoop();
+	
+	mgr.writeCommand(0x0200FF00; // Fill Rect GREEN
+	mgr.writeCommand(0x00400040; // Start pos 64,64
+	mgr.writeCommand(0x00400040; // Size 64x64 pixel.
+	for (int i=0; i<3; i++) mgr.executeInLoop();
+	
+	mgr.writeCommand(0x02FF0000; // Fill Rect BLUE.
+	mgr.writeCommand(0x00800080; // Start pos 128,128
+	mgr.writeCommand(0x00400040; // Size 64x64 pixel.
+	for (int i=0; i<3; i++) mgr.executeInLoop();
 	*/
 	
 	usleep(500000);	// 500ms Wait for the rendering to finish! TODO - Do a proper polling check for this.
@@ -171,71 +324,82 @@ ADR +12= Read Data bus (cpuDataOut), without any other CPU signal.
 	// Write to GP0...
 	// (orange diamond, from the BIOS Logo.)
 	/*
-	*((uint32_t *)axi_addr+0x000000000) = 0x380000B2;	// Color1+Command.  Shaded four-point polygon, opaque.
-	*((uint32_t *)axi_addr+0x000000000) = 0x00F000C0;	// Vertex 1.
-	*((uint32_t *)axi_addr+0x000000000) = 0x00008CB2;	// Color2.
-	*((uint32_t *)axi_addr+0x000000000) = 0x00700140;	// Vertex 2.
-	*((uint32_t *)axi_addr+0x000000000) = 0x00008CB2;	// Color3.
-	*((uint32_t *)axi_addr+0x000000000) = 0x01700140;	// Vertex 3.
-	*((uint32_t *)axi_addr+0x000000000) = 0x000000B2;	// Color4.
-	*((uint32_t *)axi_addr+0x000000000) = 0x00F001C0;	// Vertex4.
+	mgr.writeCommand(0x380000B2;	// Color1+Command.  Shaded four-point polygon, opaque.
+	mgr.writeCommand(0x00F000C0;	// Vertex 1. y=240. x=192.
+	mgr.writeCommand(0x00008CB2;	// Color2.
+	mgr.writeCommand(0x00700140;	// Vertex 2. y=112. x=320.
+	mgr.writeCommand(0x00008CB2;	// Color3.
+	mgr.writeCommand(0x01700140;	// Vertex 3. y=368. x=320.
+	mgr.writeCommand(0x000000B2;	// Color4.
+	mgr.writeCommand(0x00F001C0;	// Vertex 4. y=240. x=448.
+	for (int i=0; i<8; i++) mgr.executeInLoop();
 	*/
 	
 	//
 	// Crash Bandicoot's right ear.
 	/*
-	*((uint32_t *)axi_addr+0x000000000) = 0x30000828;	// Color1+Command.  Shaded three-point polygon, opaque.
-	*((uint32_t *)axi_addr+0x000000000) = 0x001E0019;	// Vertex 1. (YyyyXxxxh)  Y=30. X=25
-	*((uint32_t *)axi_addr+0x000000000) = 0x6000003A;	// Color2.   (00BbGgRrh)  
-	*((uint32_t *)axi_addr+0x000000000) = 0x001F0028;	// Vertex 2. (YyyyXxxxh)  Y=31. X=40
-	*((uint32_t *)axi_addr+0x000000000) = 0x60000039;	// Color3.   (00BbGgRrh)  
-	*((uint32_t *)axi_addr+0x000000000) = 0x00030001;	// Vertex 3. (YyyyXxxxh)  Y=3. X=1
-	*/
-	
-	*((uint32_t *)axi_addr+0x000000000) = 0x30FF0000;	// Color1+Command.  Shaded three-point polygon, opaque.
-	*((uint32_t *)axi_addr+0x000000000) = 0x00000000;	// Vertex 1. (YyyyXxxxh)  Y=30. X=25
-	*((uint32_t *)axi_addr+0x000000000) = 0x0000FF00;	// Color2.   (00BbGgRrh)  
-	*((uint32_t *)axi_addr+0x000000000) = 0x0000000F;	// Vertex 2. (YyyyXxxxh)  Y=31. X=40
-	*((uint32_t *)axi_addr+0x000000000) = 0x000000FF;	// Color3.   (00BbGgRrh)  
-	*((uint32_t *)axi_addr+0x000000000) = 0x000F000F;	// Vertex 3. (YyyyXxxxh)  Y=3. X=1
-	
-	/*
-	*((uint32_t *)axi_addr+0x000000000) = 0x30000828;	// Color1+Command.  Shaded three-point polygon, opaque.
-	*((uint32_t *)axi_addr+0x000000000) = 0x001E0019;	// Vertex 1. (YyyyXxxxh)  Y=30. X=25
-	*((uint32_t *)axi_addr+0x000000000) = 0x60000B3A;	// Color2.   (00BbGgRrh)  
-	*((uint32_t *)axi_addr+0x000000000) = 0x00280040;	// Vertex 2. (YyyyXxxxh)  Y=40. X=64
-	*((uint32_t *)axi_addr+0x000000000) = 0x60000B39;	// Color3.   (00BbGgRrh)  
-	*((uint32_t *)axi_addr+0x000000000) = 0x00400032;	// Vertex 3. (YyyyXxxxh)  Y=64. X=50
+	mgr.writeCommand(0x30000828;	// Color1+Command.  Shaded three-point polygon, opaque.
+	mgr.writeCommand(0x001E0019;	// Vertex 1. (YyyyXxxxh)  Y=30. X=25
+	mgr.writeCommand(0x60000B3A;	// Color2.   (00BbGgRrh)  
+	mgr.writeCommand(0x001F0028;	// Vertex 2. (YyyyXxxxh)  Y=31. X=40
+	mgr.writeCommand(0x60000B39;	// Color3.   (00BbGgRrh)  
+	mgr.writeCommand(0x00030001;	// Vertex 3. (YyyyXxxxh)  Y=3. X=1
+	for (int i=0; i<6; i++) mgr.executeInLoop();
 	*/
 	
 	/*
-	*((uint32_t *)axi_addr+0x000000000) = 0x300000FF;	// TriangleGouraud.
-	*((uint32_t *)axi_addr+0x000000000) = 0x00000000;
-	*((uint32_t *)axi_addr+0x000000000) = 0x0000FF00;
-	*((uint32_t *)axi_addr+0x000000000) = 0x0000000F;
-	*((uint32_t *)axi_addr+0x000000000) = 0x00FF0000;
-	*((uint32_t *)axi_addr+0x000000000) = 0x000F0000;
+	mgr.writeCommand(0x30FF0000;	// Color1+Command.  Shaded three-point polygon, opaque.
+	mgr.writeCommand(0x00000000;	// Vertex 1. (YyyyXxxxh)  Y=30. X=25
+	mgr.writeCommand(0x0000FF00;	// Color2.   (00BbGgRrh)  
+	mgr.writeCommand(0x0000000F;	// Vertex 2. (YyyyXxxxh)  Y=31. X=40
+	mgr.writeCommand(0x000000FF;	// Color3.   (00BbGgRrh)  
+	mgr.writeCommand(0x000F000F;	// Vertex 3. (YyyyXxxxh)  Y=3. X=1
+	for (int i=0; i<6; i++) mgr.executeInLoop();
 	*/
 	
 	/*
-	*((uint32_t *)axi_addr+0x000000000) = 0x300000B2;	// TriangleGouraud, from the BIOS logo.
-	*((uint32_t *)axi_addr+0x000000000) = 0x010301A0;
-	*((uint32_t *)axi_addr+0x000000000) = 0x00008CB2;
-	*((uint32_t *)axi_addr+0x000000000) = 0x00A0013D;
-	*((uint32_t *)axi_addr+0x000000000) = 0x00008CB2;
-	*((uint32_t *)axi_addr+0x000000000) = 0x0166013D;
+	mgr.writeCommand(0x30000828;	// Color1+Command.  Shaded three-point polygon, opaque.
+	mgr.writeCommand(0x001E0019;	// Vertex 1. (YyyyXxxxh)  Y=30. X=25
+	mgr.writeCommand(0x60000B3A;	// Color2.   (00BbGgRrh)  
+	mgr.writeCommand(0x00280040;	// Vertex 2. (YyyyXxxxh)  Y=40. X=64
+	mgr.writeCommand(0x60000B39;	// Color3.   (00BbGgRrh)  
+	mgr.writeCommand(0x00400032;	// Vertex 3. (YyyyXxxxh)  Y=64. X=50
+	for (int i=0; i<6; i++) mgr.executeInLoop();
 	*/
-
+	
+	/*
+	mgr.writeCommand(0x300000FF;	// TriangleGouraud.
+	mgr.writeCommand(0x00000000;
+	mgr.writeCommand(0x0000FF00;
+	mgr.writeCommand(0x0000000F;
+	mgr.writeCommand(0x00FF0000;
+	mgr.writeCommand(0x000F0000;
+	for (int i=0; i<6; i++) mgr.executeInLoop();
+	*/
+	
+	/*
+	mgr.writeCommand(0x300000B2;	// TriangleGouraud, from the BIOS logo.
+	mgr.writeCommand(0x010301A0;
+	mgr.writeCommand(0x00008CB2;
+	mgr.writeCommand(0x00A0013D;
+	mgr.writeCommand(0x00008CB2;
+	mgr.writeCommand(0x0166013D;
+	for (int i=0; i<6; i++) mgr.executeInLoop();
+	*/
+	
 	/*
 	for (int i=0; i<sizeof(sony_logo)/4; i++) {
-		*((uint32_t *)axi_addr+0x000000000) = sony_logo[i];
+		mgr.writeCommand(sony_logo[i];
+		mgr.executeInLoop();
+		
 	}
 	usleep(500000);	// 500ms Wait for the rendering to finish! TODO - Do a proper polling check for this.
 	*/
 	
 	/*
 	for (int i=0; i<sizeof(mem_card)/4; i++) {
-		*((uint32_t *)axi_addr+0x000000000) = mem_card[i];
+		mgr.writeCommand(mem_card[i];
+		mgr.executeInLoop();
 	}
 	usleep(500000);	// 500ms Wait for the rendering to finish! TODO - Do a proper polling check for this.
 	*/
@@ -266,9 +430,11 @@ ADR +12= Read Data bus (cpuDataOut), without any other CPU signal.
 	
 	
 	// Read flags / myDebugCnt.
+	/*
 	reg0 = *((uint32_t *)axi_addr+0x000000002);
 	printf("flag bits:  0x%01X\n", (reg0&0xF0000000)>>28 );
 	printf("myDebugCnt: 0x%08X\n", reg0&0x0FFFFFFF);
+	*/
 	
 	//*((uint32_t *)axi_addr+0x1) = 0xDEADBEEF; // Write to GP1.
 	//*((uint32_t *)axi_addr+0x2) = 0x40000000; // Write to DMA_ACK / gpu_nrst. (keep gpu_nrst HIGH!)
--- comp.sh
+++ comp.sh
@@ -1,3 +1,3 @@
-arm-linux-gnueabihf-gcc axi_test.c -o axi_test
+arm-linux-gnueabihf-g++ axi_test.c -o axi_test
 sshpass -p "1" scp axi_test root@192.168.0.76:/media/fat/
 sshpass -p '1' ssh root@192.168.0.76
